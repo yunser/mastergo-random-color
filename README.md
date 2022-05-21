03d30a78be5168369a81da8ed3060f2c9d142474
“cjhgit” <1418503647@qq.com>
2022-05-21 16:37:29 +0800

    第一版

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # mastergo-hello
 
-MasterGo 插件测试项目。
+MasterGo 随机颜色插件。
+
+![](./res/random-color-banner.png)
+
+## 插件介绍
+
+批量选中多个图层后，一键为每个图层填充随机颜色。
+
+
+## 使用方法
+
+比如选中两个矩形，然后使用插件。
--- lib/main.ts
+++ lib/main.ts
@@ -1,9 +1,98 @@
-console.log(mg.document.children)
+// console.log(mg.document.children)
+console.log('========')
+console.log('random color start')
+// console.log(__html__)
 
-console.log(__html__)
+// mg.showUI(__html__)
 
-mg.showUI(__html__)
+// console.log('mg', mg)
+// console.log('mg.document', mg.document)
+// console.log('mg.currentPage', mg.document.currentPage)
 
-mg.ui.onmessage = (msg) => {
-    console.log('onmessage', msg)
+
+if (mg.document.currentPage) {
+    console.log('currentPage')
+    const { selection } = mg.document.currentPage
+    console.log('selection', selection)
+    if (selection.length) {
+        selection.forEach((node, idx) => {
+            // console.log('node', node)
+    
+            // console.log('mg.createPaintStyle', mg.createPaintStyle)
+            // node.x = 0
+    
+            // console.log('node.fills', node.fills)
+            node.fills = [
+                {
+                    alpha: 1,
+                    blendMode: "NORMAL",
+                    color: {
+                        r: Math.random(),
+                        g: Math.random(),
+                        b: Math.random(),
+                        a: 1,
+                    },
+                    isVisible: true,
+                    type: "SOLID",
+                }
+            ]
+            // 创建一个 Paint 样式
+            // const myStyle = mg.createPaintStyle();
+            // console.log('myStyle', myStyle)
+            // // 设置样式的名称
+            // myStyle.name = "styles/fill/foo";
+            // console.log('name')
+            // // 创建一个纯色填充
+            // const paints = [
+            //     {
+            //         type: "SOLID",
+            //         color: {
+            //             r: 1,
+            //             g: 0,
+            //             b: 0,
+            //             a: 1,
+            //         },
+            //         isVisible: true,
+            //         alpha: 1,
+            //         blendMode: "NORMAL",
+            //     },
+            // ];
+            // console.log('name1')
+            // // 将该填充设置到刚刚创建的 Paint 样式上
+            // myStyle.paints = paints;
+            // console.log('name2')
+        
+            // // 最后，将刚刚创建的样式的 id 设置图层节点即可
+            // console.log('myStyle2', myStyle.id, myStyle)
+            // node.fillStyleId = myStyle.id;
+            // console.log('name3')
+        })
+    }
+    else {
+        mg.notify("请选择图层", {
+            type: "highlight",
+            position: "bottom",
+        });
+    }
+    // for (let node of selection) {
+
+
+
+    // }
+    // console.log('mg.selection', mg.document.currentPage.selection)
+
 }
+else {
+    console.log('no currentPage')
+    mg.notify("请选择图层", {
+        type: "highlight",
+        position: "bottom",
+    });
+}
+// mg.ui.onmessage = (msg) => {
+//     // console.log('onmessage', msg)
+// }
+
+mg.closePlugin()
+
+console.log('random color end')
